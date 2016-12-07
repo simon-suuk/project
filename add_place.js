fe772ad6cae6e2a467f2d83d4e4ca1f692fd48ac
simon <simon.suuk@outlook.com>
2016-12-07 14:32:15 +0000

    adding a place on map

---
+++ add_place.js
@@ -0,0 +1,46 @@
+
+function addPlace(){
+	/*
+	*the various valaues entered by the user are stored in variables
+	*/
+	var place_name = document.getElementById("place_name").value;
+	var place_lat = document.getElementById("place_lat").value;
+	var place_lng = document.getElementById("place_lng").value;
+	var website = document.getElementById("website").value;
+	var place_type = document.getElementById("place_type").value;
+	var phone = document.getElementById("phone").value;
+	var address = document.getElementById("address").value;
+	
+	/**This checks if all fields are filled*/
+	if(place_name=='' || place_lat=='' || place_lng=='' || website=='' || place_type=='' || phone=='' || address==''){
+		alert("please fill all fields");
+		return;
+	}
+	
+	var ajaxurl = 'http://52.89.116.249/~simon_baaman/project/add_place.php?place_name='+ place_name + '&place_lat='+ place_lat + '&place_lng='+ place_lng + '&website='+ website + '&place_type='+ place_type + '&phone='+ phone + '&address='+ address;
+	
+	$.ajax(
+		ajaxurl,
+		{async:true, complete:addPlaceComplete}	
+	);
+}
+
+/**
+*callback function for sendSMS ajax call
+*/
+function addPlaceComplete(xhr,status){
+	if(status!="success"){
+		
+		return;
+	}
+	
+	var response = $.parseJSON(xhr.responseText);
+	
+	if (response.status != "OK"){
+		
+		alert(response.status);
+	}else{
+		alert(response);
+		window.location.assign("http://52.89.116.249/~simon_baaman/project/index.html");
+	}
+}
---
+++ add_place.php
@@ -0,0 +1,53 @@
+<?php
+if (isset($_REQUEST['place_lat'])) {
+			
+	function ProcessCurl($URL, $fieldString){ //Initiate Curl request and send back the result
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+		curl_setopt($ch, CURLOPT_URL, $URL);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($ch, CURLOPT_VERBOSE, 1);
+		//curl_setopt($ch, CURLOPT_POST, 1);
+		
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldString);
+		
+		$resulta = curl_exec ($ch);
+		
+		if (curl_errno($ch)) {
+			print curl_error($ch);
+		} else {
+		curl_close($ch);
+		}
+		echo $resulta;
+	}
+	
+	$place_name = $_REQUEST['place_name'];
+	$place_lat = $_REQUEST['place_lat'];
+	$place_lng = $_REQUEST['place_lng'];
+	$website = $_REQUEST['website'];
+	$place_type = $_REQUEST['place_type'];
+	$phone = $_REQUEST['phone'];
+	$address = $_REQUEST['address'];
+
+	$jsonpost = '{
+		"location": {
+			"lat": $place_lat,
+			"lng": $place_lng
+		},
+		"accuracy": 50,
+		"name": $place_name,
+		"phone_number": $phone, 
+		"address": $address, 
+		"types": [$place_type], 
+		"website": $website, 
+		"language": "en-AU"
+	}';
+
+	$url = "https://maps.googleapis.com/maps/api/place/add/json?sensor=false&key=AIzaSyAIycY6g4-PkvM1xgwezfp5qnKLzKB3eXA";
+
+	$results = ProcessCurl ($url, $jsonpost);
+
+	echo $results;
+}
+?>
