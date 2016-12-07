212685d3df937a26a6367877aba3c77509261a83
simon <simon.suuk@outlook.com>
2016-12-07 11:03:05 +0000

    contacts using sqlite 3

--- login.js
+++ login.js
@@ -32,73 +32,57 @@ function loginComplete(xhr,status){
 		alert(response.message);
 	}else{
 		
-		$(document).ready(function(){
-			var myDB;
-			//Open Database Connection
-			document.addEventListener("deviceready",onDeviceReady,false);
-			function onDeviceReady(){
-				myDB = window.sqlitePlugin.openDatabase({name: "mySQLite.db", location: 'default'});
-				
-				navigator.contacts.find([navigator.contacts.fieldType.displayName],gotContacts,errorHandler);
-			}
+		var myDB;
+		
+		document.addEventListener("deviceready", init, false);
+		function init() {
+			myDB = window.sqlitePlugin.openDatabase({name: "mySQLite.db", location: 'default'});
+			navigator.contacts.find([navigator.contacts.fieldType.displayName],gotContacts,errorHandler);
+		}
+		function errorHandler(e) {
+			console.log("errorHandler: "+e);
+		}
+		
+		//Insert New Data
+		function gotContacts(c) {
+			/* Retriving phoneNumbers */
+			for(var i=0, len=c.length; i<len; i++) {
+				if(c[i].phoneNumbers && c[i].phoneNumbers.length > 0) {
+					myDB.transaction(function(transaction) {
+					var executeQuery = "INSERT INTO user_contacts (contact_name, mobile_num) VALUES (?,?)";  
 
-			//Create new table
-			myDB.transaction(function(transaction) {
-			transaction.executeSql('CREATE TABLE IF NOT EXISTS user_contacts (id integer primary key autoincrement, contact_name text, mobile_num text)', [],
-				function(tx, result) {
-					alert("Table created successfully");
-				}, 
-				function(error) {
-					  alert("Error occurred while creating the table.");
-				});
-			});
-
-			//Insert New Data
-			function gotContacts(c) {
-				/* Retriving phoneNumbers */
-				for(var i=0, len=c.length; i<len; i++) {
-					if(c[i].phoneNumbers && c[i].phoneNumbers.length > 0) {
-						myDB.transaction(function(transaction) {
-						var executeQuery = "INSERT INTO user_contacts (contact_name, mobile_num) VALUES (?,?)";  
-
-						transaction.executeSql(executeQuery, [c[i].displayName,c[i].phoneNumbers[0].value]
-							, function(tx, result) {
-								 alert('Inserted');
-							},
-							function(error){
-								 alert('Error occurred'); 
-							});
+					transaction.executeSql(executeQuery, [c[i].displayName,c[i].phoneNumbers[0].value]
+						, function(tx, result) {
+							 alert('Inserted');
+						},
+						function(error){
+							 alert('Error occurred'); 
 						});
-					}
+					});
 				}
 			}
-
-			function errorHandler(e) {
-				console.log("errorHandler: "+e);
-			}
-
-			//Display Table Data
-			myDB.transaction(function(transaction) {
-			transaction.executeSql('SELECT * FROM phonegap_pro', [], function (tx, results) {
-			   var len = results.rows.length, i;
-			 
-			   for (i = 0; i < len; i++){
-				  alert(results.rows.item(i).contact_name+" "+results.rows.item(i).mobile_num);
-			   }
-			}, null);
-			});
-
-
-			myDB.transaction(function(transaction) {
-				var executeQuery = "DROP TABLE  IF EXISTS user_contacts";
-				transaction.executeSql(executeQuery, [],
-					function(tx, result) {alert('Table deleted successfully.');},
-					function(error){alert('Error occurred while droping the table.');}
-				);
-			});
+		}
+		
+		//Display Table Data
+		myDB.transaction(function(transaction) {
+		transaction.executeSql('SELECT * FROM user_contacts', [], function (tx, results) {
+		   var len = results.rows.length, i;
+		 
+		   for (i = 0; i < len; i++){
+			  alert(results.rows.item(i).contact_name+" "+results.rows.item(i).mobile_num);
+		   }
+		}, null);
 		});
 
 
+		/*myDB.transaction(function(transaction) {
+			var executeQuery = "DROP TABLE  IF EXISTS user_contacts";
+			transaction.executeSql(executeQuery, [],
+				function(tx, result) {alert('Table deleted successfully.');},
+				function(error){alert('Error occurred while droping the table.');}
+			);
+		});*/
+		
 		window.location.assign("http://52.89.116.249/~simon_baaman/project/index.html");
 	}
 }
