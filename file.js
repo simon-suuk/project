be11ec06901426627715d6babecc51142d0bebdc
simon <simon.suuk@outlook.com>
2016-12-08 10:49:21 +0000

    filesystem

---
+++ file.js
@@ -0,0 +1,17 @@
+function createFile() {
+   var type = window.TEMPORARY;
+   var size = 5*1024*1024;
+
+   window.requestFileSystem(type, size, successCallback, errorCallback)
+
+   function successCallback(fs) {
+      fs.root.getFile('log.txt', {create: true, exclusive: true}, function(fileEntry) {
+         alert('File creation successfull!')
+      }, errorCallback);
+   }
+
+   function errorCallback(error) {
+      alert("ERROR: " + error.code)
+   }
+	
+}
